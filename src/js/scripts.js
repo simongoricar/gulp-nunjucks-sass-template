bcf052413014a668495564115a27c3fbb4a4ffd9
sebastianaclane <sebastianaclane@gmail.com>
2020-09-14 11:21:45 -0700

    Sebastian pair programming exercise

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,7 +3,18 @@
   'use strict';
 
   $(function () {
-
+    //js code
+    // 4. Determine what radio button is selected (event listener)
+    // 5. Swap pricing cards from monthly to yearly or vise versa
+    $('fieldset.form-group').click(function() {
+      if (this.dataset.default) {
+        // show monthlyPricing cards setting display object with css
+      } else {
+        // show yearlPricing cards setting display object with css
+      }
+      console.log(this.dataset.default);
+    });
+    // nunjucks code is "{{}}"
   });
 
 })(jQuery, window, document);
