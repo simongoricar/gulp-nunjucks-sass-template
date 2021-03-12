0e3ceed01f8b40868a5eddafcdb9310afd99f315
Simon Goričar <simon.peter.goricar@gmail.com>
2021-03-12 18:43:40 +0100

    Add css file renaming

--- tasks/task-css.ts
+++ tasks/task-css.ts
@@ -4,12 +4,18 @@
 import sassCompiler from "sass";
 import sass from "gulp-sass";
 import sourcemaps from "gulp-sourcemaps";
+import gulpRename from "gulp-rename";
 import { src as gulpSrc, dest as gulpDest } from "gulp";
 
 import mainConfig from "./configuration";
 
-export default (): Node.ReadWriteStream => gulpSrc(mainConfig.css.srcEntry)
+export default (): unknown => gulpSrc(mainConfig.css.srcEntry)
+    .pipe(gulpRename("style.min.css"))
     .pipe(sourcemaps.init())
-    .pipe(sass({ compiler: sassCompiler, outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(
+        sass(
+            { compiler: sassCompiler, outputStyle: "compressed" },
+        ).on("error", sass.logError),
+    )
     .pipe(sourcemaps.write(mainConfig.css.sourcemapsDir))
     .pipe(gulpDest(mainConfig.css.outputDir));
