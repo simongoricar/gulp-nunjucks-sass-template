9a67101135051290ade0ac4d836706de96b32733
Simon Goričar <simon.peter.goricar@gmail.com>
2021-03-12 19:34:47 +0100

    Add other asset copy task

--- tasks/index.ts
+++ tasks/index.ts
@@ -7,10 +7,12 @@ import gulp from "gulp";
 import taskScripts from "./task-scripts";
 import taskCss from "./task-css";
 import taskHtml from "./task-html";
+import taskCopyOtherAssets from "./task-other-assets";
 
 export const scripts = taskScripts;
 export const css = taskCss;
 export const html = taskHtml;
+export const otherAssets = taskCopyOtherAssets;
 
 // noinspection JSUnusedGlobalSymbols
-export default gulp.parallel(scripts, css, html);
+export default gulp.parallel(scripts, css, html, otherAssets);
---
+++ tasks/task-other-assets.ts
@@ -0,0 +1,16 @@
+/**
+ * Gulp task: Other assets
+ */
+import { src as gulpSrc, dest as gulpDest, lastRun as gulpLastRun } from "gulp";
+
+import { AsyncTask } from "async-done";
+
+import mainConfig from "./configuration";
+
+export default function copyOtherAssets(): ReturnType<AsyncTask> {
+    return gulpSrc(
+        `${mainConfig.otherAssets.srcDir}/**/*`,
+        { since: gulpLastRun(copyOtherAssets) },
+    )
+        .pipe(gulpDest(mainConfig.otherAssets.outputDir));
+}
